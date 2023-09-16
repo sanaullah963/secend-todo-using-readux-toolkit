4da0cd05481b940a2bd4e20664f1e230df8e2b1b
sanaullah963 <allhera963@gmail.com>
2023-09-17 01:45:26 +0600

    header banner file added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
-import Header from '@/components/Header'
-import Image from 'next/image'
-
+import Banner from '@/components/Banner'
 export default function Home() {
   return (
     <main>
-      
+      <Banner/>
     </main>
   )
 }
---
+++ src/components/Banner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div>this is home page</div>
+  )
+}
+
+export default Banner
